a683072a7da3860e8eea1f78dd970002d869a810
Allan Johnson <allanjohnson.leal@gmail.com>
2022-02-01 15:58:25 -0300

    feat: add products entity model

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import { STATUS_CODES } from 'http';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' });
+routes.get('/health', (request, response) => {
+  return response.json({
+    data: { message: 'Health OK!', status: 200 },
+  });
 });
 
 export default routes;
